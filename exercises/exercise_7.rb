50a05509bb381522b907e5ac552bae3867b06c98
Alex Cheung <alexc.sfu@gmail.com>
2022-06-27 23:39:34 -0700

    completed exercise_6.rb and exercise_7.rb

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,12 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+
+# @store2.employees.create(hourly_rate: 35).errors.each {|e| puts e.full_message}
+
+puts "Give me a store name:"
+print "> "
+input = $stdin.gets.chomp
+
+Store.create(name: input).errors.each {|e| puts e.full_message}
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,18 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+
+  validates :name, presence: true, length: {minimum: 3}
+  validates :annual_revenue, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+  validates :mens_apparel, :womens_apparel, inclusion:{in: [true, false], message: "- Please input true or false."}
+
+  validate :at_least_one_type_of_apparel
+
+  def at_least_one_type_of_apparel
+    if !mens_apparel.present? && !womens_apparel.present?
+      errors.add("Store","must carry at least one of the men's or women's apparel")
+    end
+  end
 
 end
+
+
