979a8726c33b092fc11b4d30c08a02aec413db0d
Alex Cheung <alexc.sfu@gmail.com>
2022-06-27 20:42:13 -0700

    completed exercise_4.rb

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,14 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+surrey = Store.create(name: 'Surrey', annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+
+whistler = Store.create(name: 'Whistler', annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+
+Yaletown = Store.create(name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+# printing all the store names which match the serach result
+Store.where(mens_apparel: true).each{|e| puts e.name}
+
+Store.where(["womens_apparel = '%s' and annual_revenue < '%s'", "true", "1000000"]).each{|e| puts e.name}
